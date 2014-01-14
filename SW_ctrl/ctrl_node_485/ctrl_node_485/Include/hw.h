36ab1bfc48519b44cf60ac48e16cf3984e2edc80
unknown <dhajtas@DHAJTAS-L03.invcorp.invensense.com>
2014-01-14 16:47:41 +0100

    pin definitions

--- SW_ctrl/ctrl_node_485/ctrl_node_485/Include/hw.h
+++ SW_ctrl/ctrl_node_485/ctrl_node_485/Include/hw.h
@@ -7,29 +7,27 @@
 #define FOSC				16000
 #define LITTLE_ENDIAN
 
-#define SPI_P				B
-#define SPI_MOSI			PB2
-#define SPI_MISO			PB3
-#define SPI_SS				PB0
-#define SPI_SCK				PB1
+#define OW_P				C
+#define OW_DATA				PC4
 
-#define FAN_P				B
-#define FAN					PB7
-#define FAN_PWM				OC1C
+#define RS_P				D
+#define RS_TX				PD1
+#define RS_RX				PD0
+#define RS_DE				PD2
 
-#define DIAL_P				B
-#define DIAL_UP				PB6
-#define DIAL_DOWN			PB5
-#define DIAL_BUTTON			PB4
+#define PUMP_P				C
+#define PUMP				PC5
+#define PUMP_PWM			OC1C
 
-#define STOP_P				C
-#define STOP				PC7
-#define STOP_INT			INT4
-#define STOP_INT_VECT		INT4_vect
+#define ZCD_P				D
+#define ZCD					PD3
+#define ZCD_INT				INT1
+#define ZCD_INT_VECT		INT1_vect
 
-#define TEMP_P				D
-#define	TEMP0				PD6
-#define	TEMP1				PD7
+#define MIX_P				C
+#define	MIX_CW				PC2
+#define	MIX_CCW				PC3
+#define MIX_IN				ADC6
 
 #define LED_P				D
 #define LED0				PD2
@@ -46,10 +44,23 @@
 #define PHASE				PC4
 #define PHASE_INT			PCINT10
 
-#define DISP_P				D
-#define DISP_SCL			PD1
-#define DISP_SDA			PD0
-#define DISP_RST_P			C
-#define DISP_RST			PC2
+#define KEY_P				B
+#define KEY_COM				PB5
+#define KEY_UP				PB1
+#define KEY_DOWN			PB4
+#define KEY_LEFT			PB3
+#define KEY_RIGHT			PB2
+
+#define DISP_PL				B
+#define DISP_PH				D
+#define DISP_A				PB2
+#define DISP_B				PB4
+#define DISP_C				PD4
+#define DISP_D				PD6
+#define DISP_E				PD5
+#define DISP_F				PB3
+#define DISP_G				PB1
+#define DISP_DIG1			PD7
+#define DISP_DIG2			PB0
 
 #endif //HW_H
